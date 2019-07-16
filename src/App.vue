2b94d9112620d93e49063f738674a92c0939dbf6
elasticdotventures <35611074+elasticdotventures@users.noreply.github.com>
2019-07-16 23:10:35 +1000

    asdf

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>
@@ -10,6 +11,7 @@
         -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <!--
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -17,29 +19,49 @@
       >
         <span class="mr-2">Latest Release</span>
       </v-btn>
+      -->
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="secondary"
+          dark
+          v-on="on"
+        >
+          Menu
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click=""
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>        
     </v-toolbar>
+
 
     <v-parallax dark height="300" src="img/background-circuit1.jpg">
     <h1>Elastic.Ventures</h1>
-  
-
       <v-img
           :src="require('./assets/elasticventures_text_svg.svg')"
           class="my-3"
           contain
           height="100"
         ></v-img>
-
-
     </v-parallax>
 
 
     <v-content>
             <HelloWorld/>
     </v-content>
+
     <v-footer>
       <Footer />
     </v-footer>
+
   </v-app>
 </template>
 
@@ -56,10 +78,13 @@ export default {
     HelloWorld,
     Footer
   },
-  data () {
-    return {
-      //
-    }
-  }
+  data: () => ({
+    items : [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ]
+  })
 }
 </script>
