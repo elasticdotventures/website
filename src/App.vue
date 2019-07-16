6f56241da6e8d8d52da7fbe1b5d38b5bf7af101e
elasticdotventures <35611074+elasticdotventures@users.noreply.github.com>
2019-07-16 11:30:06 +1000

    Label App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>App.vue</span>
+        <!--
+          <span class="font-weight-light">MATERIAL DESIGN</span>
+        -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
