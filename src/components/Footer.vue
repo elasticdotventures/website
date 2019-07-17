26e06f9c9de996dc77fab0bc219ca4057a54d78c
elasticdotventures <35611074+elasticdotventures@users.noreply.github.com>
2019-07-17 18:13:21 +1000

    working on adding tos & privacy policy (not operational)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,34 @@
 <template>
-<v-footer>
-	<h1>Footer</h1>
-	Copyright 2019 - Elastic.Ventures. All rights reserved.
-						| <a style="color: white" href="tos.txt">Terms of Service</a>
-						| <a style="color: white" href="privacypolicy.htm">Privacy Policy</a>
-</v-footer>
+  <v-footer>
+	  <!--
+	   icons https://material.io/tools/icons/?style=outline
+	  -->
+	  <v-icon>copyright</v-icon>
+    2019 - Elastic Ventures, Inc. 
+    <EVTermsOfService v-if="showTermsOfService" />
+    <div v-else>
+	  <v-icon>home</v-icon>
+      <a href="tos.txt">Terms of Service</a>
+    </div>
+
+    <EVPrivacyPolicy v-if="showPrivacyPolicy" />
+    <div v-else>
+	  <v-icon>bookmark</v-icon>
+      <a href="privacypolicy.htm">Privacy Policy</a>
+    </div>
+  </v-footer>
 </template>
+
+<script>
+//import EVTermsOfService from "components/_Dataphiles/EVTermsOfService.vue";
+//import EVPrivacyPolicy from "components/_Dataphiles/EVPrivacyPolicy.vue";
+
+export default {
+  data() {
+    return {
+      showTermsOfService: false,
+      showPrivacyPolicy: false
+    };
+  }
+};
+</script>
