85df53272c7ce3771f07fc869e0687c4607c3624
Brian Horakh <brianh@elastic.ventures>
2019-07-24 14:10:07 +1000

    /topic attempting to fix PostCSS on @margaret

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   publicPath: '/',
-    chainWebpack: (config) => {
+  chainWebpack: (config) => {
         config.resolve.symlinks(false)
-          }
+  },
+  devServer: {
+    clientLogLevel: 'debug',
+    compress: false,
+    contentBase: './dist'
+  }
 }
