ee6126b889abf5a76aefc142273a1265ff630061
elasticdotventures <35611074+elasticdotventures@users.noreply.github.com>
2019-07-31 21:57:17 +1000

    social icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,3 +5,34 @@ import 'vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   iconfont: 'md',
 })
+
+// import { mdiAccount } from '@mdi/js'
+
+/*
+   material design icons  
+   https://www.npmjs.com/package/vue-material-design-icons
+
+example from https://vuetifyjs.com/en/framework/icons
+<template>
+  <v-icon>$vuetify.icons.product</v-icon>
+  <v-icon v-text="'$vuetify.icons.support'"></v-icon>
+  <v-icon v-html="'$vuetify.icons.steam'"></v-icon>
+  <v-icon v-text="platform"></v-icon>
+</template>
+
+NOTE: this library provides the mdi-xxxx series (facebook, etc.)
+*/
+import '@mdi/font/css/materialdesignicons.css'
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+  icons: {
+    'pretty':'mdi-steam-box',  // 
+    'product': 'mdi-dropbox',
+    'support': 'mdi-lifebuoy',
+    'steam': 'mdi-steam-box',
+    'pc': 'mdi-desktop-classic',
+    'xbox': 'mdi-xbox',
+    'playstation': 'mdi-playstation',
+    'switch': 'mdi-nintendo-switch'
+  }
+})
