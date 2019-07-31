011155f6b722eacde78e58127368038aa9e92fd0
elasticdotventures <35611074+elasticdotventures@users.noreply.github.com>
2019-07-31 19:48:59 +1000

    cleanup

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
 
+import Router from 'vue-router'
 Vue.use(Router)
 
 let component1 = {
@@ -19,7 +18,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('./views/Home.vue')
     },
     {
       path: '/about',
